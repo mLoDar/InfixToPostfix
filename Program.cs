4825e9f41d02076d3055aaaa7291a5f2885c3ce1
mLoDar <mlodarcoding@gmail.com>
2024-10-15 12:11:05 +0200

    Added main instruction menu Created a simple menu which displays instructions and reads an input from the console.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
-﻿namespace InfixToPostfix
+﻿using System.Text;
+
+
+
+
+
+namespace InfixToPostfix
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+        LabelMethodEntry:
+
+            Console.Title = "InfixToPostfix";
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            Console.Clear();
+            Console.SetCursorPosition(0, 4);
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("              ┳  ┏•    ┏┳┓    ┏┓    ┏•               ");
+            Console.WriteLine("              ┃┏┓╋┓┓┏   ┃ ┏┓  ┃┃┏┓┏╋╋┓┓┏             ");
+            Console.WriteLine("              ┻┛┗┛┗┛┗   ┻ ┗┛  ┣┛┗┛┛┗┛┗┛┗             ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("             ───────────────────────────────         ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                                     ");
+            Console.WriteLine("             ┌ Please enter an infix term:           ");
+            Console.WriteLine("             └────────────────────────────┐          ");
+            Console.WriteLine("                                                     ");
+            Console.Write("             > ");
+
+            string inputInfixTerm = Console.ReadLine() ?? string.Empty;
+
+            
+
+
+
+            goto LabelMethodEntry;
         }
     }
 }
